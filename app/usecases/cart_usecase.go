5c4cdecc180756c235a7d8573cbf45a8a753c7ad
Ateto1202 <tonysu1204@gmail.com>
2024-11-23 14:57:38 +0800

    Create cart usecase

--- app/usecases/cart_usecase.go
+++ app/usecases/cart_usecase.go
@@ -1,1 +1,27 @@
 package usecases
+
+import (
+	"github.com/we-we-Web/dongyi-cart-serv/app/domain"
+	"github.com/we-we-Web/dongyi-cart-serv/app/repository"
+)
+
+type CartUseCase interface {
+	Save(userID string) (*domain.Cart, error)
+	GetByID(cartID string) (*domain.Cart, error)
+}
+
+type cartUseCase struct {
+	repo *repository.CartRepository
+}
+
+func NewCartUseCase(repo *repository.CartRepository) CartUseCase {
+	return &cartUseCase{repo}
+}
+
+func (uc *cartUseCase) Save(userID string) (*domain.Cart, error) {
+	return nil, nil
+}
+
+func (uc *cartUseCase) GetByID(cartID string) (*domain.Cart, error) {
+	return nil, nil
+}
